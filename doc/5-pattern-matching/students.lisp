e6a2c5c92668f21206bee49305c662e448504304
Matthew Barber <quitesimplymatt@gmail.com>
2017-12-23 02:52:34 +0000

    done lab 5 to room counting tasks, where I accidently went ahead and added a clause to count bedrooms individually as well

---
+++ doc/5-pattern-matching/students.lisp
@@ -0,0 +1,3 @@
+(defun tut () (load "tutorial5.lisp"))
+
+(setf students `((dave 81) (dee 26) (dozy 58) (beaky 52) (mick 45) (tich 64)))
---
+++ sln/5/tutorial5.lisp
@@ -0,0 +1,34 @@
+; be sure to load house-db.lisp first to access the variable
+
+(defun tut () (load "tutorial5.lisp"))
+
+(setf students `((dave 81) (dee 26) (dozy 58) (beaky 52) (mick 45) (tich 64)))
+
+(defun sort-students (students min max)
+  (let ((query nil))
+    (dolist (stu-mark students query)
+      (let ((mark (second stu-mark)))
+	(when (and (< min mark) (> max mark))
+	  (setf query (cons stu-mark query)))))))
+
+(defun get-property-value (property association-list)
+  (second (assoc property association-list)))
+
+(defun get-rooms (house)
+  (get-property-value `rooms house))
+
+(defun get-room (room house)
+  (get-property-value room (get-rooms house)))
+
+(defun get-room-count (houses)
+  (let ((house-counts nil))
+    (dolist (house houses house-counts)
+     (setf house-counts
+      (cons (list (get-property-value `name house)
+       (let ((count 0))
+	 (dolist (room (get-rooms house) count)
+	   (if (equal `bedrooms (first room))
+	       (dolist (bedroom (second room) count)
+		 (incf count 1))
+	     (incf count 1))))) house-counts)))))
+	
